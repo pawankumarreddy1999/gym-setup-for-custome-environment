e8f6e6f57cb3fca1e0628ab16a9687fee161c036
Pavan Kumar Reddy Elluru <78949569+pawankumarreddy1999@users.noreply.github.com>
2021-06-04 18:14:18 +0530

    Update __init__.py

--- cus_env/__init__.py
+++ cus_env/__init__.py
@@ -1,6 +1,15 @@
 from gym.envs.registration import register
+from copy import deepcopy
+
+from . import datasets
+
 
 register(
-    id='env_name-v0',
-    entry_point='cus_env.envs:CustomEnv',
+    id='simplestock-v0',
+    entry_point='cus_env.envs:CusEnv',
+    kwargs={
+        'df': deepcopy(datasets.HDFC),
+        'window_size': 24,
+        'frame_bound': (24, len(datasets.HDFC))
+    }
 )
